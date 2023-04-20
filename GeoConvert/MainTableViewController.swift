9479bba5b6b4f157d2ada2403e696d2859bbfecb
mrpaschenko <paschenkod2@gmail.com>
2023-04-20 17:36:36 +0200

    Create sections in table

--- GeoConvert/MainTableViewController.swift
+++ GeoConvert/MainTableViewController.swift
@@ -22,24 +22,25 @@ class MainTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 2
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 2
     }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FieldCell", for: indexPath)
 
+        cell.textLabel?.text = "Hello world \(indexPath.row)"
         // Configure the cell...
 
         return cell
     }
-    */
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Section \(section)"
+    }
 
     /*
     // Override to support conditional editing of the table view.
